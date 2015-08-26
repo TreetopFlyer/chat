6b05bc82afb2b252b97b56daa1e3812793385824
Seth Trowbridge <seth111@gmail.com>
2015-08-25 20:25:24 -0400

    separate on data handlers

--- server.js
+++ server.js
@@ -22,49 +22,71 @@ function ParseHeader(inHeaderString)
 };
 var serverTcp = net.createServer(function(inConnection)
 {
-	inConnection.FirstTime = true;
 	inConnection.Name = "";
-	inConnection.on("error", function(inError){
+	
+	
+	inConnection.HandlerDataHandshake = function(inData)
+	{
+		var parts = inData.toString().split("\r\n");
+		var pieces = [];
+		var i;
+		var header = {};
+		var colonIndex;
+		var key, value;
+		for(i=0; i<parts.length; i++)
+		{
+			colonIndex = parts[i].indexOf(": ");
+			if(colonIndex != -1)
+			{
+				key = parts[i].substring(0, colonIndex);
+				value = parts[i].substring(colonIndex+2);
+				header[key] = value;
+			}
+		}
+		
+		SHAHasher = crypto.createHash("sha1");
+		SHAHasher.update(header["Sec-WebSocket-Key"]);
+		SHAHasher.update("258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+
+		var output = "";
+		output += "HTTP/1.1 101 Switching Protocols"+"\r\n";
+		output += "Upgrade: websocket"+"\r\n";
+		output += "Connection: Upgrade"+"\r\n";
+		output += "Sec-WebSocket-Accept: "+SHAHasher.digest("base64")+"\r\n";
+		output += ""+"\r\n";
+		
+		inConnection.write(output);
+		
+		//now switch to the other handler, no need for handshake stuff after this
+		inConnection.HandlerData = inConnection.HandlerDataMessage;		
+	};
+	inConnection.HandlerDataMessage = function(inData)
+	{
+		// this is for <128 character messages
+		var blitFlag = inData[1] & 0x80; // this is a flag, not the actual mask
+		var length = inData[1] & 0x7f;
+		var blit = inData.slice(2, 6);
+		var message = inData.slice(6, 6+length);
+		var i;
+		if(blitFlag === 128)
+		{
+			for(i=0; i<message.length; i++)
+			{
+				message[i] = message[i] ^ blit[i%4];
+			}
+		}
+		console.log(message.toString());
+	};
+	inConnection.HandlerData = inConnection.HandlerDataHandshake;
+	
+	
+	inConnection.on("error", function(inError)
+	{
 		console.log(inError);
 	});
 	inConnection.on("data", function(inData)
 	{
-		console.log(inConnection.FirstTime);
-		if(inConnection.FirstTime)
-		{
-			inConnection.FirstTime = false;
-			console.log("i see a new client");
-			var obj = ParseHeader(inData.toString());
-
-			SHAHasher = crypto.createHash("sha1");
-			SHAHasher.update(obj["Sec-WebSocket-Key"]);
-			SHAHasher.update("258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
-
-			var output = "";
-			output += "HTTP/1.1 101 Switching Protocols"+"\r\n";
-			output += "Upgrade: websocket"+"\r\n";
-			output += "Connection: Upgrade"+"\r\n";
-			output += "Sec-WebSocket-Accept: "+SHAHasher.digest("base64")+"\r\n";
-			output += ""+"\r\n";
-			
-			inConnection.write(output);
-		}
-		else
-		{
-			// this is for <128 character messages
-			var blitFlag = inData[1] & 0x80; // this is a flag, not the actual mask
-			var length = inData[1] & 0x7f;
-			var blit = inData.slice(2, 6);
-			var message = inData.slice(6, 6+length);
-			var i;
-			if(blitFlag === 128)
-			{
-				for(i=0; i<message.length; i++)
-				{
-					message[i] = message[i] ^ blit[i%4];
-				}
-			}
-		}
+		inConnection.HandlerData(inData);
 	});
 	inConnection.on("end", function()
 	{
