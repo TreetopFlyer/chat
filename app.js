7796e3593e928898fa8d9d26dec16881bd966ac4
Seth Trowbridge <seth111@gmail.com>
2015-08-25 21:28:37 -0400

    bootstrapping

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express");
+var handlebars = require("express-handlebars");
+
+
 var app = express();
-
-app.set("view engine", "awfawf");
-
+app.engine("html", handlebars());
+app.set("view engine", "html");
+app.set("views", __dirname+"/views");
+app.use("/files", express(__dirname+"files"));
 app.get("/", function(inRequest, inResponse, inNext)
 {
-	inResponse.send("root");
-	inNext();
+	inResponse.render("index", {title:"cHat"});
 });
 
 
